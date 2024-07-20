8bc8e36764966d3dcab19e9d1df8bcfee4575545
Jose Burgos <jose.30819564@uru.edu>
2024-07-20 16:40:23 -0400

    actividades b.o

--- backend/B.O/Actividades.js
+++ backend/B.O/Actividades.js
@@ -2,6 +2,10 @@ import { iPgHandler } from "../data/psql-data/iPgManager.js";
 
 export class Actividades{
     static async crearActividad({projectId, nombre, objective, startDate, endDate}){
-
+        try {
+            
+        } catch (error) {
+            
+        }
     }
 }
